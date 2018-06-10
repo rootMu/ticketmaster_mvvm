845782b97e88a533991cf70d0bd8e8e8aef39582
Matthew Howells <matthewhowells@live.com>
2018-06-10 14:32:12 +0100

    base application

---
+++ app/src/main/java/com/example/matthew/ticketmaster_mvvm/TicketMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.matthew.ticketmaster_mvvm
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+/**
+ * Created by Matthew on 10/06/2018.
+ */
+class TicketMasterApplication {
+
+}
