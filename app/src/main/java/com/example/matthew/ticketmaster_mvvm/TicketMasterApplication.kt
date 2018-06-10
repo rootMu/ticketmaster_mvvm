518004d99688986a076c9e58e5f11c2d238dba91
Matthew Howells <matthewhowells@live.com>
2018-06-10 14:36:12 +0100

    initial application

--- app/src/main/java/com/example/matthew/ticketmaster_mvvm/TicketMasterApplication.kt
+++ app/src/main/java/com/example/matthew/ticketmaster_mvvm/TicketMasterApplication.kt
@@ -1,8 +1,5 @@
 package com.example.matthew.ticketmaster_mvvm
 
-import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
-
 /**
  * Created by Matthew on 10/06/2018.
  */
